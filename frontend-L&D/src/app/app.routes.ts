24437e5044162f42d89d288afc04f414cee48e3c
preetha-2002-eng <124424334+preetha-2002-eng@users.noreply.github.com>
2025-06-19 14:52:21 +0530

    Delete frontend-L&D directory

--- frontend-L&D/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { EmployeeComponent } from './employee/employee.component';
-
-export const routes: Routes = [
-    { path: '', component: EmployeeComponent }
-];
